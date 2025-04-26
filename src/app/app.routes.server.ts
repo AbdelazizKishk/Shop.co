d57d15f2b3919ce9c81deb15eff2c774f499bd7f
Abdelaziz mohamed <abdelazizkishk19@gmail.com>
2025-04-26 17:11:51 +0300

    commit

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    path: 'checkout/:id',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'detailsprod/:id',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'detailsCatg/:category',
+    renderMode: RenderMode.Prerender,
+  },
 ];
